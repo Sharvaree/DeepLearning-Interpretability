28b7a1a9e9f0db1403706237670fb892ffba4294
Mohanraj DS <52766071+mrajds@users.noreply.github.com>
2019-12-05 20:00:06 -0700

    Added Embeddings for the words from the paper.
    ReadEmbeddings loads the BertEmbeddings.txt file as a dictionary

---
+++ ReadEmbeddings.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def ReadEmbeddingsFile():
+  EmbedDict = {}
+  f = open('BertEmbeddings.txt','r')
+  txt = f.read()
+  lines = txt.splitlines()
+  for line in lines:
+    words = line.split()
+    EmbedArr = np.array([float(x) for x in(words[1:])])
+    EmbedDict[words[0]] = EmbedArr
+  return EmbedDict
+  
+if __name__ == '__main__':
+  EmbedDict = ReadEmbeddingsFile()
+  print(EmbedDict["uncle"].shape)
